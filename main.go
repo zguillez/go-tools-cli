70070a2ef6c02b5f6c3e5b0f6eb3414d12375bbd
Guillermo de la Iglesia <zguillez@gmail.com>
2021-11-11 12:40:47 +0100

    deploy

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 		gotools.Help()
 	} else if *version {
 		gotools.Version()
-		color.Cyan("[gtools-cli v0.1.9]")
+		color.Cyan("[gtools-cli v0.1.10]")
 	} else if *command != "" {
 		var args []string
 		switch *command {
